c4bf0d88f87596d30a2107b1f19f0c43deb8c577
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-09-19 16:21:47 -0400

    - Created the FSharpx.Compatibility.OCaml.System project to hold re-implemented modules for interacting with the system/environment. This is a separate project because it'll need to reference some extra assemblies (e.g., Mono.Unix) that aren't necessary for the main project. - Created the FSharpx.Compatibility.OCaml.Format project to hold a ported version of the OCaml Format module. This is a separate project because we're actually porting the OCaml code (instead of re-implementing it), so the code needs to be licensed under the LGPL (unlike the other projects in this solution, which are licensed under the Apache 2.0 license).

---
+++ FSharpx.Compatibility.OCaml.Format/Format.fs
@@ -0,0 +1,16 @@
+﻿(*  OCaml Compatibility Library for F#
+    (FSharpx.Compatibility.OCaml)
+
+    Copyright © 2012 Jack Pappas (github.com/jack-pappas)
+
+    This code is available under the GNU Lesser
+    General Public License (LGPL) v2. *)
+
+//
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module FSharpx.Compatibility.OCaml.Format
+
+// TODO
+
+let dummy () = ()
+
---
+++ FSharpx.Compatibility.OCaml.System/Sys.fs
@@ -0,0 +1,16 @@
+﻿(*  OCaml Compatibility Library for F#
+    (FSharpx.Compatibility.OCaml)
+
+    Copyright © 2012 Jack Pappas (github.com/jack-pappas)
+
+    This code is available under the Apache 2.0 license.
+    See the LICENSE file for the complete text of the license. *)
+
+//
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module FSharpx.Compatibility.OCaml.Sys
+
+// TODO
+
+let dummy () = ()
+
