6fbb03bfe149166ef3b331b789d95cc044ed1091
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-07 21:00:32 -0400

    - Created a test project for FSharpx.Compatibility.OCaml.

---
+++ FSharpx.Compatibility.OCaml.Tests/Num.fs
@@ -0,0 +1,28 @@
+﻿(*  OCaml Compatibility Library for F#
+    (FSharpx.Compatibility.OCaml)
+
+    Copyright © 2012 Jack Pappas (github.com/jack-pappas)
+
+    This code is available under the Apache 2.0 license.
+    See the LICENSE file for the complete text of the license. *)
+
+module FSharpx.Compatibility.OCaml.Tests.Num
+
+open NUnit.Framework
+open FsUnit
+
+open System
+open System.Numerics
+open Microsoft.FSharp.Math
+open FSharpx.Compatibility.OCaml.Num
+
+
+[<Test>]
+let ``0 does not equal 1``() =
+    (Int 0) = (Int 1)
+    |> should equal false
+
+
+
+
+
