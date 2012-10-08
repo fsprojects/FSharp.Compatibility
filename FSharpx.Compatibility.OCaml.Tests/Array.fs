052d58b979dfdc968c75c28b4c20444d6f8a0b6c
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-07 21:07:06 -0400

    - Created an empty test module for Array.

---
+++ FSharpx.Compatibility.OCaml.Tests/Array.fs
@@ -0,0 +1,18 @@
+﻿(*  OCaml Compatibility Library for F#
+    (FSharpx.Compatibility.OCaml)
+
+    Copyright © 2012 Jack Pappas (github.com/jack-pappas)
+
+    This code is available under the Apache 2.0 license.
+    See the LICENSE file for the complete text of the license. *)
+
+module FSharpx.Compatibility.OCaml.Tests.Array
+
+open NUnit.Framework
+open FsUnit
+
+open FSharpx.Compatibility.OCaml.Array
+
+
+// TODO : Add test cases.
+
