dc5a6aacbc758fdaaa239c9e4e6a8f0b1e1ca351
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-11-27 15:49:16 -0500

    - Added AssemblyInfo.fs for the StandardML library. - Created .nuspec for the StandardML library.

---
+++ FSharp.Compatibility.StandardML/AssemblyInfo.fs
@@ -0,0 +1,35 @@
+﻿(*
+Copyright (c) 2012, Jack Pappas
+All rights reserved.
+
+This code is provided under the terms of the 2-clause ("Simplified") BSD license.
+See LICENSE.TXT for licensing details.
+*)
+
+module internal AssemblyInfo
+
+open System
+open System.Diagnostics.CodeAnalysis
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open System.Security.Permissions
+
+
+[<assembly: AutoOpen("FSharp.Compatibility.StandardML")>]
+[<assembly: CLSCompliant(true)>]
+
+#if FX_NO_SECURITY_PERMISSIONS
+#else
+#if FX_SIMPLE_SECURITY_PERMISSIONS
+[<assembly: SecurityPermission(SecurityAction.RequestMinimum)>]
+#else
+#endif
+#endif
+
+#if FX_NO_DEFAULT_DEPENDENCY_TYPE
+#else
+[<assembly: Dependency("FSharp.Core", LoadHint.Always)>] 
+#endif
+
+do ()
