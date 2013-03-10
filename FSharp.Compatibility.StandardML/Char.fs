dc69c5727a61b272c8fc9f57226ff60dd8a362f5
Jack Pappas <jackp@server.fake>
2013-03-09 23:10:06 -0500

    Implemented functions in the Char, String, and ListPair modules.

---
+++ FSharp.Compatibility.StandardML/Char.fs
@@ -0,0 +1,135 @@
+﻿(*
+
+Copyright 2013 Jack Pappas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+// Reference:
+// http://www.standardml.org/Basis/char.html
+
+//
+module FSharp.Compatibility.StandardML.Char
+
+(*
+
+eqtype char
+eqtype string
+
+val minChar : char
+val maxChar : char
+val maxOrd : int
+
+val ord : char -> int
+val chr : int -> char
+val succ : char -> char
+val pred : char -> char
+
+val compare : char * char -> order
+val <  : char * char -> bool
+val <= : char * char -> bool
+val >  : char * char -> bool
+val >= : char * char -> bool
+
+*)
+
+/// returns the (non-negative) integer code of the character c.
+let inline ord (c : char) =
+    int c
+
+/// returns the character whose code is i; raises Chr if i < 0 or i > maxOrd.
+let chr (i : int) =
+    raise <| System.NotImplementedException "Char.chr"
+
+/// returns true if character c occurs in the string s; otherwise it returns false.
+let contains (s : string) (c : char) =
+    s.IndexOf c <> -1
+
+//
+let notContains (s : string) (c : char) =
+    s.IndexOf c = -1
+
+/// returns true if c is a (seven-bit) ASCII character, i.e., 0 <= ord c <= 127.
+/// Note that this function is independent of locale.
+let isAscii (c : char) =
+    int c <= 127
+
+/// These return the lowercase (respectively, uppercase) letter corresponding to c if c is a letter; otherwise it returns c.
+let inline toLower (c : char) =
+    System.Char.ToLowerInvariant c
+
+//
+let inline toUpper (c : char) =
+    System.Char.ToUpperInvariant c
+
+/// returns true if c is a letter (lowercase or uppercase).
+let inline isAlpha (c : char) =
+    System.Char.IsLetter c
+
+/// returns true if c is alphanumeric (a letter or a decimal digit).
+let inline isAlphaNum (c : char) =
+    System.Char.IsLetterOrDigit c
+
+/// returns true if c is a control character.
+let inline isCntrl (c : char) =
+    System.Char.IsControl c
+
+/// returns true if c is a decimal digit [0-9].
+let inline isDigit (c : char) =
+    System.Char.IsDigit c
+
+/// returns true if c is a graphical character, that is, it is printable and not a whitespace character.
+let isGraph (c : char) =
+    not <| System.Char.IsWhiteSpace c
+
+/// returns true if c is a hexadecimal digit [0-9a-fA-F].
+let isHexDigit (c : char) =
+    match c with
+    | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    | 'a' | 'b' | 'c' | 'd' | 'e' | 'f'
+    | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' -> true
+    | _ -> false
+
+/// returns true if c is a lowercase letter.
+let inline isLower (c : char) =
+    System.Char.IsLower c
+
+/// returns true if c is a printable character (space or visible), i.e., not a control character.
+let isPrint (c : char) =
+    raise <| System.NotImplementedException "Char.isPrint"
+
+/// returns true if c is a whitespace character (space, newline, tab, carriage return, vertical tab, formfeed).
+let isSpace (c : char) =
+    System.Char.IsWhiteSpace c
+
+/// returns true if c is a punctuation character: graphical but not alphanumeric.
+let isPunct (c : char) =
+    System.Char.IsPunctuation c
+
+//
+let inline isUpper (c : char) =
+    System.Char.IsUpper c
+
+//
+let toString (c : char) =
+    // TODO : The semantics defined for this method need to be handled correctly
+    // -- we can't just call .ToString().
+    raise <| System.NotImplementedException "Char.toString"
+
+(*
+val scan       : (Char.char, 'a) StringCvt.reader -> (char, 'a) StringCvt.reader
+val fromString : String.string -> char option
+val toCString : char -> String.string
+val fromCString : String.string -> char option
+*)
---
+++ FSharp.Compatibility.StandardML/Substring.fs
@@ -0,0 +1,77 @@
+﻿(*
+
+Copyright 2013 Jack Pappas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+// Reference:
+// http://www.standardml.org/Basis/substring.html
+
+//
+module FSharp.Compatibility.StandardML.Substring
+
+// string
+// dropl
+// dropr
+// full
+
+
+(*
+
+type substring
+eqtype char
+eqtype string
+
+val sub : substring * int -> char
+val size : substring -> int
+val base : substring -> string * int * int
+val extract   : string * int * int option -> substring
+val substring : string * int * int -> substring
+val full : string -> substring
+val string : substring -> string
+val isEmpty : substring -> bool
+val getc : substring -> (char * substring) option
+val first : substring -> char option
+val triml : int -> substring -> substring
+val trimr : int -> substring -> substring
+val slice : substring * int * int option -> substring
+val concat : substring list -> string
+val concatWith : string -> substring list -> string
+val explode : substring -> char list
+val isPrefix    : string -> substring -> bool
+val isSubstring : string -> substring -> bool
+val isSuffix    : string -> substring -> bool
+val compare : substring * substring -> order
+val collate : (char * char -> order)
+                -> substring * substring -> order
+val splitl : (char -> bool)
+                -> substring -> substring * substring
+val splitr : (char -> bool)
+                -> substring -> substring * substring
+val splitAt : substring * int -> substring * substring
+val dropl : (char -> bool) -> substring -> substring
+val dropr : (char -> bool) -> substring -> substring
+val takel : (char -> bool) -> substring -> substring
+val taker : (char -> bool) -> substring -> substring
+val position : string -> substring -> substring * substring
+val span : substring * substring -> substring
+val translate : (char -> string) -> substring -> string
+val tokens : (char -> bool) -> substring -> substring list
+val fields : (char -> bool) -> substring -> substring list
+val app : (char -> unit) -> substring -> unit
+val foldl : (char * 'a -> 'a) -> 'a -> substring -> 'a
+val foldr : (char * 'a -> 'a) -> 'a -> substring -> 'a
+
+*)
