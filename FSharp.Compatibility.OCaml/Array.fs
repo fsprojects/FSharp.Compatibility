572bd7517afaa6ecda1b45608f4d8d226ef32d7c
Jack Pappas <jackp@server.fake>
2013-03-23 15:32:19 -0400

    Updated the license text to the actual text from the Apache 2.0 license.

--- FSharp.Compatibility.OCaml/Array.fs
+++ FSharp.Compatibility.OCaml/Array.fs
@@ -1,12 +1,21 @@
-﻿(*  OCaml Compatibility Library for F#
-    (FSharp.Compatibility.OCaml)
+﻿(*
 
-    Copyright (c) Microsoft Corporation 2005-2009
-    Copyright (c) Jack Pappas 2012
-        http://github.com/jack-pappas
+Copyright 2005-2009 Microsoft Corporation
+Copyright 2012 Jack Pappas
 
-    This code is distributed under the terms of the Apache 2.0 license.
-    See the LICENSE file for details. *)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
 
 // Reference:
 // http://caml.inria.fr/pub/docs/manual-ocaml/libref/Array.html
