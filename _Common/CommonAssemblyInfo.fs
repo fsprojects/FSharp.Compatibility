bad2c500b22f5d91df2ea8e41ac2b2987fa0d071
Jack Pappas <jackp@server.fake>
2013-07-07 11:41:30 -0400

    Increment version numbers to 0.1.11.

--- _Common/CommonAssemblyInfo.fs
+++ _Common/CommonAssemblyInfo.fs
@@ -49,9 +49,9 @@ let [<Literal>] private assemblyConfig =
 [<assembly: ComVisible(false)>]
 
 // Version information
-[<assembly: AssemblyVersion("0.1.10")>]
-[<assembly: AssemblyFileVersion("0.1.10")>]
-[<assembly: AssemblyInformationalVersion("0.1.10")>]
+[<assembly: AssemblyVersion("0.1.11")>]
+[<assembly: AssemblyFileVersion("0.1.11")>]
+[<assembly: AssemblyInformationalVersion("0.1.11")>]
 
 // Only allow types derived from System.Exception to be thrown --
 // any other types should be automatically wrapped.
