7652839ca67531a18263b9e9ab8502f9358d73a6
Jack Pappas <jackp@server.fake>
2014-07-20 11:24:52 -0400

    Import the Mono.Posix NuGet package into the FSharp.Compatibility.OCaml.System project so it can be used when implementing the Unix module.

---
+++ FSharp.Compatibility.OCaml.System/Unix.fs
@@ -0,0 +1,33 @@
+﻿(*
+
+Copyright 2014 Jack Pappas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+/// Interface to the Unix system.
+[<CompilerMessage(
+    "This module is for ML compatibility. This message can be disabled using '--nowarn:62' or '#nowarn \"62\"'.",
+    62, IsHidden = true)>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module FSharp.Compatibility.OCaml.Unix
+
+open Mono.Unix
+
+
+// TODO
+
+//exception Unix_error of error * string * string
+
+
